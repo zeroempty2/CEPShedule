e9e7dd9b549fc0e88f1e3a97cdfa58a1e310bfe1
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-09-29 19:07:01 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/example/cepscheduler/CepSchedulerApplication.java
+++ src/main/java/com/example/cepscheduler/CepSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.example.cepscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CepSchedulerApplication {
 
   public static void main(String[] args) {
